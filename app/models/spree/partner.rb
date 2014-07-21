d86af60521122399776ccca261a74a9de8e72466
RostDetal <info@rostdetal.ru>
2014-07-21 22:41:02 +0400

    Партнеры

---
+++ app/models/spree/partner.rb
@@ -0,0 +1,2 @@
+class Spree::Partner < Spree::Base
+end
--- db/migrate/20140721114603_create_partners.rb
+++ db/migrate/20140721114603_create_partners.rb
@@ -1,9 +1,10 @@
 class CreatePartners < ActiveRecord::Migration
   def change
-    create_table :partners do |t|
+    create_table :spree_partners do |t|
       t.string :name
       t.string :phone
       t.string :unique
+      t.string :site_url
 
       t.timestamps
     end
