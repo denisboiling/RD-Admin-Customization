c15d5d36b3c0dfbcb4f4d4cd2f81bc8a87f9ecb7
RostDetal <info@rostdetal.ru>
2014-07-22 08:42:07 +0400

    Партнеры

--- app/overrides/partners_add_to_admin.rb
+++ app/overrides/partners_add_to_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(virtual_path:  "spree/admin/shared/_configuration_menu",
                      name:          "pages_admin_partners_menu",
                      insert_bottom: "[data-hook='admin_configurations_sidebar_menu'], #admin_configurations_sidebar_menu[data-hook]",
-                     text:          "<%= configurations_sidebar_menu_item 'Партнеры', admin_partners_path %>",
+                     text:          "<%= configurations_sidebar_menu_item Spree.t(':partners'), admin_partners_path %>",
                      disabled:      false)
