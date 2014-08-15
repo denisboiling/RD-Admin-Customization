590e15734990f91c75f4a65822bf9db78d053beb
RostDetal <info@rostdetal.ru>
2014-08-15 14:37:39 +0400

    Правки в админке

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -1,9 +1,9 @@
 #coding: UTF-8
 module Spree
   AppConfiguration.class_eval do
-	preference :global_site_email, :string, default: 'info@rostdetal.ru'
-	preference :global_site_phone, :string, default: '+7(905)457-72-57'
-	preference :global_site_location, :string, default: 'г. Ростов на Дону'
+	preference :global_site_email, :string, default: 'info@example.com'
+	preference :global_site_phone, :string, default: '+0(000)000-00-00'
+	preference :global_site_location, :string, default: 'г. Москва'
 	preference :global_home, :string, default: "/"
 	preference :global_blog, :string, default: "/blog"
 	preference :global_forum, :string, default: "/forum"
