32d8d78e36246d7ca99272990cca0a16e4c830dc
RostDetal <info@rostdetal.ru>
2014-09-09 22:20:37 +0400

    партнерка

--- db/migrate/20140909153945_add_partner_field_to_spree_products.rb
+++ db/migrate/20140909153945_add_partner_field_to_spree_products.rb
@@ -1,6 +1,9 @@
+# This migration comes from spree_admin_customization (originally 20140909153945)
 class AddPartnerFieldToSpreeProducts < ActiveRecord::Migration
   def change
-    add_column :spree_products, :partner_id, :int
-    add_index :spree_products, :partner_id
+    if !table_exists?(:spree_products)
+      add_column :spree_products, :partner_id, :int
+      add_index :spree_products, :partner_id
+    end
   end
 end
