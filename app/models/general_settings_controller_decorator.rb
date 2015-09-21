3fe4ab58af5233e8d7e7be62f6e20c4e433d9899
NikolaevAV <nuriko@inbox.ru>
2015-09-21 10:57:27 +0300

    Фикс ошибки в админке

--- app/models/general_settings_controller_decorator.rb
+++ app/models/general_settings_controller_decorator.rb
@@ -2,12 +2,7 @@ module Spree
   module Admin
     GeneralSettingsController.class_eval do
       def edit
-        @preferences_general = [:site_name, :default_seo_title, :default_meta_keywords,
-                                :default_meta_description, :site_url, :global_site_phone,:global_site_location,:global_site_email]
-        @preferences_security = [:allow_ssl_in_production,
-                                 :allow_ssl_in_staging, :allow_ssl_in_development_and_test,
-                                 :check_for_spree_alerts]
-        @preferences_currency = [:display_currency, :hide_cents]
+        @preferences_security = []
       end
     end
   end
