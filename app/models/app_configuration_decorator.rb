76a50303e2dd3aaa08df60a6b46f1caf343d437e
denis <denis@genesix.ru>
2015-10-01 18:20:36 +0200

    fix

--- app/models/app_configuration_decorator.rb
+++ app/models/app_configuration_decorator.rb
@@ -2,8 +2,8 @@
 module Spree
   AppConfiguration.class_eval do
 	preference :global_site_email, :string, default: 'info@example.com'
-	preference :global_site_phone, :string, default: '+0(000)000-00-00'
-	preference :global_site_location, :string, default: 'г. Москва'
+	preference :global_site_phone, :string, default: '8 800 123-45-67'
+	preference :global_site_location, :string, default: 'Москва'
 	preference :global_home, :string, default: "/"
 	preference :global_blog, :string, default: "/blog"
 	preference :global_forum, :string, default: "/forum"
